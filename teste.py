2159bcad13bc4f2359b0d3935bde7cab7025253c
Gustavo Januário <gustavo.sjanuario@gmail.com>
2022-10-08 14:47:07 -0300

    update teste.py

---
+++ teste.py
@@ -0,0 +1,45 @@
+import speech_recognition as sr
+
+import os
+
+
+# Função para ouvir e reconhecer a fala
+def ouvir_microfone():
+    # habilita o microfone do usuário
+    global frase
+    microfone = sr.Recognizer()
+
+    # usando o microfone
+    with sr.Microphone() as source:
+
+        # chama um algoritmo de redução de ruidos no som
+        microfone.adjust_for_ambient_noise(source)
+
+        # frase para o usuário dizer algo
+        print('Diga alguma coisa: ')
+
+        # armazena o que foi dito numa variável
+        audio = microfone.listen(source)
+
+    try:
+
+        # passa a variável para o algoritmo reconhecedor de padrões
+        frase = microfone.recognize_google(audio, language='pt-BR')
+
+        if 'navegador' in frase:
+            os.system('start Chrome.exe')
+
+        elif 'Excel' in frase:
+            os.system('start Excel.exe')
+
+        # retorna a frase pronunciada
+        print('Você disse: ' + frase)
+
+    # se não recoheceu o padrão de fal, exibe a mensagem
+    except sr.UnknownValueError:
+        print('Não entendi')
+
+        return frase
+
+
+ouvir_microfone()
